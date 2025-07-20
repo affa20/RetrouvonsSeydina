e5a6df85155a136116d9a7b2fb4345a9d0f17f06
affa20 <60344890+affa20@users.noreply.github.com>
2025-07-20 23:45:45 +0200

    Add files via upload

---
+++ js/geoloc.js
@@ -0,0 +1,21 @@
+﻿window.getGeolocation = () => {
+    return new Promise((resolve, reject) => {
+        if (!navigator.geolocation) {
+            reject("Geolocation non supportée");
+        } else {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    resolve({
+                        coords: {
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude
+                        }
+                    });
+                },
+                error => {
+                    reject(error.message);
+                }
+            );
+        }
+    });
+};
